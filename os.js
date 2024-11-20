2b31565b8c7d465c58685c0c285c3ea264822a8d
optanwar <optanwar10@gmail.com>
2024-11-21 02:51:31 +0530

    fs moduel

--- os.js
+++ os.js
@@ -1,5 +1,10 @@
+const fs = require('fs');
+
+const text = fs.readFileSync('./text.txt','utf-8');
+
+console.log(text);
 
 
-const os = require('os')
+const data = `Hi my name is ${text} amd created at ${new Date().getFullYear()}`;
 
-console.log('os :>> ', os);
+fs.writeFileSync('./textq.txt', data);
